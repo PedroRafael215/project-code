45a4bbdc681961e24186a94c3db34b8b34cac6bf
Pedro Albano <pedro.rafael215@gmail.com>
2023-11-25 20:04:10 -0300

    Acho que agora foi - Stable

--- src/index.js
+++ src/index.js
@@ -1,86 +1,87 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+
 const telegramBot = require('node-telegram-bot-api');
 const TelegramBot = require('node-telegram-bot-api/lib/telegram');
 require('dotenv').config();
+
 const TOKEN = process.env.TOKEN;
-
-let Sensor = "";
-let setSensor = 'on';
-
+let botInitialized = false;
+let BOT;
+let chatId;
 const app = express();
 const port = 3000;
 
-var d = new Date(); 
-let dataAtual = d.toLocaleString('pt-BR');
 
+let getChatIdMessage= "ChatId Obtido";
 
-const bot = new telegramBot(TOKEN, {polling: true});
-
+if (botInitialized == false) {
+  botInitialized = true;
+  //botInitialized = true;
+  const BOT = new TelegramBot(TOKEN, { polling: true });
+  bot = BOT;
+  bot.on('message', (message) => {
+    chatId = message.from.id;
+    //let userMessage = message.text;
+  
+    bot.sendMessage(chatId, getChatIdMessage)
+  .then(() => {
+    console.log('Mensagem do GetChatID enviada com sucesso.');
+  })
+  .catch((error) => {
+    console.error('Erro ao enviar mensagem do do GetChatID para o bot:', error);
+  });
+});
+  console.log("Bot Initialized");
+}
 
 
 app.use(bodyParser.json());
-
 app.post('/arduino-sensor', (req, res) => {
+  res.status(200).json({ mensagem: `Valor de detection recebido: ${req.body[0].detection}` });
   
   // Verifica se a variável "detection" está presente no corpo da requisição
-  //console.log('Variável:' + req.body[0].detection);
   
   if (req.body[0] && req.body[0].detection) {
-    const valorDetection = req.body[0].detection;
-    res.status(200).json({ mensagem: `Valor de detection recebido: ${valorDetection}` });
+    console.log('Variável:' + req.body[0].detection);
+
+
+    if(req.body[0].detection == "-DETECTED-"){
+      
+    console.log('Variável:' + req.body[0].detection);
+      enviarMensagemAoBot();
+      
+  }
   } else {
     res.status(400).json({ erro: 'Variável "detection" ausente no corpo da requisição JSON' });
   }
-
-  // if (req.body && req.body.detection) {
-  //   const valorDetection = req.body.detection;
-  //   res.status(200).json({ mensagem: `Valor de detection recebido: ${valorDetection}` });
-  // } else {
-  //   res.status(400).json({ erro: 'Variável "detection" ausente no corpo da requisição JSON' });
-  // }
-
-  Sensor = req.body[0].detection;
-  setSensor = 'on';
-//-----------------------------------------
-
 });
+
+async function enviarMensagemAoBot() {
+  var d = new Date(); 
+let dataAtual = d.toLocaleString('pt-BR');
+    // Envia uma mensagem para um chat específico ou para todos os chats, dependendo dos requisitos
+    const chatIdBot = chatId; // Substitua pelo ID real do chat
+
+    // Mensagem a ser enviada
+    const mensagem = "ALERTA\n\nAlarme acionado em:\n" + dataAtual;
+
+    // Envia a mensagem
+    await bot.sendMessage(chatIdBot, mensagem)
+      .then(() => {
+        console.log('Mensagem enviada com sucesso.');
+      })
+      .catch((error) => {
+        console.error('Erro ao enviar mensagem para o bot:', error);
+      });
+  }
+
 
 app.listen( 
- {
-  host:'0.0.0.0',
-  port: process.env.PORT? Number(process.env.PORT): 3333,
-  
-});
-
-console.log('HTTP Server Running');
-
-
-  if(!bot.isPolling){
-  bot.on('message', (message) => {
-      console.log("Pedro says: "+ message.text);
-      //console.log(message.from.id);
-  
-      let chatId = message.from.id;
-      let userMessage = message.text;
-
-      if( userMessage == "/command2" )
-      {
-        setSensor = 'off'
-      }
-      if( userMessage == "/command1" )
-      {
-        setSensor = 'on';
-      }
-      if( Sensor == "-DETECTADO-" && setSensor != 'off' )
-      {
-        bot.sendMessage("ALERTA\n\nAlarme acionado em:\n");
-      }
-      if(userMessage != "/command1" || userMessage != "/command2")
-      {
-          bot.sendMessage(chatId, 'Por favor, especifique um comando válido disponível');
-          bot.sendMessage(chatId, `/command1 - para Habilitar o Sensor\n/command2 - para Desabilitar o Sensor`);
-      }
-  
-  });
-}
+  {
+   host:'0.0.0.0',
+   port: process.env.PORT? Number(process.env.PORT): 3333,
+   
+ });
+ 
+ console.log('HTTP Server Running');
