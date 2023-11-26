c9a5831b1237074e4bcef200f725d5b9102af8ca
Pedro Albano <pedro.rafael215@gmail.com>
2023-11-26 11:41:05 -0300

    beta 2.0 stable

--- src/index.js
+++ src/index.js
@@ -7,76 +7,22 @@ require('dotenv').config();
 
 const TOKEN = process.env.TOKEN;
 let botInitialized = false;
-let BOT;
+var bot;
 let chatId = 716656717;
 const app = express();
 const port = 3000;
 
-
 let getChatIdMessage= "ChatId Obtido";
 
-if (botInitialized == false) {
-  botInitialized = true;
-  //botInitialized = true;
-  const BOT = new TelegramBot(TOKEN, { polling: true });
-  bot = BOT;
-  bot.on('message', (message) => {
-    chatId = message.from.id;
-    //let userMessage = message.text;
-  
-    bot.sendMessage(chatId, getChatIdMessage)
-  .then(() => {
-    console.log('Mensagem do GetChatID enviada com sucesso.');
-  })
-  .catch((error) => {
-    console.error('Erro ao enviar mensagem do do GetChatID para o bot:', error);
-  });
-});
-  console.log("Bot Initialized");
-}
-
-
-app.use(bodyParser.json());
-app.post('/arduino-sensor', (req, res) => {
-  //console.log('teste--------');
-  //console.log(req.body[0].value);
-  //console.log('teste--------');
-  
-  //console.log('teste2--------');
-  //console.log("DEBUGGGGGG " + req.body[0] );
-  //console.log('teste2--------');
-
-  res.status(200).json({ mensagem: `Valor de detection recebido: ${req.body[0].value}` });
-  
-  // Verifica se a variável "detection" está presente no corpo da requisição
-  
-  if (req.body[0].value== 'DETECTED') {
-    console.log('Variável:' + req.body[0].value);
-console.log('CHATID ' + chatId);
-
-    if(req.body[0].value == 'DETECTED'){
-      
-    console.log('Variável:' + req.body[0].value);
-      enviarMensagemAoBot();
-      
-  }
-  
-  } else {
-   console.log("Variavél detection não presente. erro");// res.status(400).json({ erro: 'Variável "detection" ausente no corpo da requisição JSON' });
-  }
-});
-
-async function enviarMensagemAoBot() {
- 
+async function enviarMensagemAoBot() 
+{ 
   if (Number(chatId)){
     var d = new Date().toLocaleString("en-US", {timeZone: "America/Sao_Paulo"});
     let dataAtual = d.toLocaleString('pt-BR');
     // Envia uma mensagem para um chat específico ou para todos os chats, dependendo dos requisitos
     const chatIdBot = chatId; // Substitua pelo ID real do chat
-
     // Mensagem a ser enviada
     const mensagem = "ALERTA\n\nAlarme acionado em:\n" + dataAtual;
-
     // Envia a mensagem
     await bot.sendMessage(chatIdBot, mensagem)
       .then(() => {
@@ -88,9 +34,43 @@ async function enviarMensagemAoBot() {
   }else{
     console.log("chatID not Defined yet");
   }
-  
-  }
+}
 
+if (botInitialized == false) {
+   botInitialized = true;
+   bot = new TelegramBot(TOKEN, { polling: true });
+   console.log("Bot Initialized");
+}
+
+bot.on('message', (message) => {
+  chatId = message.from.id;
+  //let userMessage = message.text;
+  bot.sendMessage(chatId, getChatIdMessage)
+.then(() => {
+  console.log('Mensagem do GetChatID enviada com sucesso.');
+  console.log('chatID: ' + chatId);
+})
+.catch((error) => {
+  console.error('Erro ao enviar mensagem do do GetChatID para o bot:', error);
+});
+});
+
+app.use(bodyParser.json());
+app.post('/arduino-sensor', (req, res) => {
+    
+  res.status(200).json({ mensagem: `Valor de detecção recebido: ${req.body[0].value}` });
+  
+  if (req.body[0].value== 'DETECTED') 
+   {
+    console.log('Variável:' + req.body[0].value);
+    enviarMensagemAoBot();
+    console.log('CHATID ' + chatId);
+   }
+   else 
+   {
+   console.log("Variavél detection não presente. erro");// res.status(400).json({ erro: 'Variável "detection" ausente no corpo da requisição JSON' });
+   }
+});
 
 app.listen( 
   {
